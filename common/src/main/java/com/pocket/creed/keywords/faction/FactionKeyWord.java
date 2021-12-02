3ed52a883ab2aefd3935dff7a0afc8b17f49dfb2
Bartosz Nowak <bacik12211@gmail.com>
2021-12-02 22:25:34 +0100

    Adding enums for "Adeptus Custodes" faction.

---
+++ common/src/main/java/com/pocket/creed/keywords/faction/FactionKeyWord.java
@@ -0,0 +1,9 @@
+package com.pocket.creed.keywords.faction;
+
+public enum FactionKeyWord {
+    IMPERIUM,
+    CHAOS,
+    XENOS,
+    AELDARI,
+    UNALIGNED
+}
--- common/src/main/java/com/pocket/creed/keywords/faction/imperium/ImperiumKeyWord.java
+++
@@ -1,4 +0,0 @@
-package com.pocket.creed.keywords.faction.imperium;
-
-public enum ImperiumKeyWord {
-}
